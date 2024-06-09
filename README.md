794cd91b1655f9250ed2196c8b43c96b1de3bfd2
Ninglo <jiujianian@gmail.com>
2024-06-09 13:13:51 +0800

    pub

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# joystickMapper
-手柄映射到键盘
+This is a simple demo of using the [Gamepad API](http://www.w3.org/TR/gamepad/) in JavaScript. It attempts to account for differences in implementations of the unfinished spec, as well as Chrome's prefixed implementation.
 
-最近Ryujinx出了针对M1的版本，正好公司提供的工作设备是M1 pro~
-在玩ns模拟器的时候，发现一般的手柄并不受模拟器兼容，根本不识别。只能键盘的ns不是想要的ns。
+Gamepad API Test
 
-尝试将手柄映射到键盘，这种项目之前有一些，比如enjoyable，效果不好，需要sudo才能有效的使用，另外，他造成了口袋妖怪游戏异常，包括但不仅限于游戏中商店不能对话，不能存档，不能战斗中选择不同招式。经过确认，通过键盘来玩的时候确实没有出现。所以我觉得大概率是键盘映射者造成的，但是那个是好多年以前的oc写的，很多api都废弃了，也不好调试。
+Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
 
-所以决定用py写一个简单的自己先用。
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
--- test.py
+++ test.py
@@ -1,7 +1,17 @@
+import threading
 import pygame
 # 参考文档 https://www.pygame.org/docs/ref/joystick.html?highlight=gamepad
 
 import json
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
 with open("config.json",'r') as f:
     config  = json.load(f)
@@ -16,30 +26,38 @@ print(joystick_count)
 # stick.init()
 
 from pynput.keyboard import Key, Controller
+from pynput.mouse import Controller as Mouse, Button
 keyboard = Controller()
+mouse = Mouse()
 # 用于映射一些键盘不能按出来的按键
 def getKeyByStr(str):
     keyMap = {
+        'page_up':Key.page_up,
+        'page_down':Key.page_down,
         'up':Key.up,
         'down':Key.down,
         'left':Key.left,
         'right':Key.right,
-
     }
 
     if str in keyMap.keys():
         return keyMap[str]
     
     # 没找到就给个ese吧
-    return Key.esc
+    return str
 
-
+timer = None
 # clock = pygame.time.Clock()
 done = False
+
+# while True:
+#     if 
+#     pass
+
 while not done:
     try:
         for event in pygame.event.get():
-            print(event)
+            # print(event)
             if event.type == pygame.JOYDEVICEADDED:
                 stick = pygame.joystick.Joystick(0)
                 stick.init()
@@ -48,57 +66,59 @@ while not done:
                     continue
                 k = config['button'][event.button]
                 if k != "":
-                    keyboard.press(k)
+                    keyboard.press(getKeyByStr(k))
             if event.type == pygame.JOYBUTTONUP:
                 if event.button >= len(config['button']):
                     continue
                 k = config['button'][event.button]
                 if k != "":
-                    keyboard.release(k)
+                    keyboard.release(getKeyByStr(k))
             if event.type == pygame.JOYAXISMOTION:
                 if event.axis >= len(config['axis']):
                     continue
-                if event.value < -0.3:
+                if event.axis <= 3:
+                    continue
+                
+                print(event)
+                if event.value < -0.5:
                     k = config['axis'][event.axis][0]
-                    k1 = config['axis'][event.axis][1]
-                    keyboard.press(k)
-                    keyboard.release(k1)
-                if event.value > 0.3:
+                    keyboard.press(getKeyByStr(k))
+                    # keyboard.release(getKeyByStr(k))
+                if event.value > 0.5:
                     k = config['axis'][event.axis][1]
-                    k1 = config['axis'][event.axis][0]
-                    keyboard.press(k)
-                    keyboard.release(k1)
+                    # keyboard.press(getKeyByStr(k))
+                    keyboard.release(getKeyByStr(k))
                 if event.value == 0:
                     for k in config['axis'][event.axis]:
-                        keyboard.release(k)
+                        keyboard.release(getKeyByStr(k))
             if event.type == pygame.JOYHATMOTION:
                 x,y = event.value 
                 
                 if x == -1:
                     kStr = config['hat'][0][0]
                     k = getKeyByStr(kStr)
-                    keyboard.press(k)
+                    keyboard.press(getKeyByStr(k))
                 if x == 1:
                     kStr = config['hat'][0][1]
                     k = getKeyByStr(kStr)
-                    keyboard.press(k)
+                    keyboard.press(getKeyByStr(k))
                 if x == 0:
                     for kStr in config['hat'][0]:
                         k = getKeyByStr(kStr)
-                        keyboard.release(k)
+                        keyboard.release(getKeyByStr(k))
                 
                 if y == -1:
                     kStr = config['hat'][1][0]
                     k = getKeyByStr(kStr)
-                    keyboard.press(k)
+                    keyboard.press(getKeyByStr(k))
                 if y == 1:
                     kStr = config['hat'][1][1]
                     k = getKeyByStr(kStr)
-                    keyboard.press(k)
+                    keyboard.press(getKeyByStr(k))
                 if y == 0:
                     for kStr in config['hat'][1]:
                         k = getKeyByStr(kStr)
-                        keyboard.release(k)
+                        keyboard.release(getKeyByStr(k))
 
                 
     except KeyboardInterrupt:
